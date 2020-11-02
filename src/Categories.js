48638be4870b3a4334914007ad21fb0ea3854537
Keir <mcglencoe@hotmail.fr>
2020-11-02 19:51:15 +0100

    Create categories component + export icons

---
+++ src/Categories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Categories.css";
+
+let urls = [
+  "concerts.png",
+  "events.png",
+  "mariage.png",
+  "medieval.png",
+  "nature.png",
+  "personal.png",
+  "portraits.png",
+  "shows.png",
+];
+
+function Categories() {
+  return (
+    <div class="iconcontainer">
+      {urls.map((url) => (
+        <div className="item">
+          <img
+            src={"./categories/" + url}
+            className="categories-image"
+            alt="categories"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Categories;
