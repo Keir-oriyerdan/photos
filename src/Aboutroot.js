2c0b92b464cc44d9bbbfe8518b6119e5c2993a27
Keir <mcglencoe@hotmail.fr>
2020-11-11 23:14:29 +0100

    Fixed About.css

--- src/Aboutroot.js
+++ src/Aboutroot.js
@@ -6,7 +6,7 @@ function Aboutroot() {
   return (
     <div className="all">
       <img className="title" src={portrait} alt="portrait" />
-      <p className="text">
+      <p className="lorem">
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
