8d5f42cb98063f0103ed635557695fe51244a6a4
Keir <mcglencoe@hotmail.fr>
2020-11-19 21:09:44 +0100

    Test to fixed Css form.css

--- src/Form.js
+++ src/Form.js
@@ -22,23 +22,25 @@ class Form extends React.Component {
     return (
       <div className="form">
         <img className="sidepic" src={background} alt="homepage" />
-        {JSON.stringify(this.state.nom)}
-        <label htmlFor="Name">Nom :</label>
-        <input
-          type="text"
-          id="nom"
-          name="nom"
-          value={this.state.nom}
-          onChange={this.handleChange}
-          multiple
-        />
-        <label htmlFor="Name">Prenom :</label>
-        <input type="text" id="prenom" name="prenom" />
-        <select value={this.state.nom}>
-          <option value="demo1">Démo 1</option>
-          <option value="demo2">Démo 2</option>
-          <option value="demo3">Démo 3</option>
-        </select>
+        <div className="formbody">
+          {JSON.stringify(this.state.nom)}
+          <label htmlFor="Name">Nom :</label>
+          <input
+            type="text"
+            id="nom"
+            name="nom"
+            value={this.state.nom}
+            onChange={this.handleChange}
+            multiple
+          />
+          <label htmlFor="Name">Prenom :</label>
+          <input type="text" id="prenom" name="prenom" />
+          <select value={this.state.nom}>
+            <option value="demo1">Démo 1</option>
+            <option value="demo2">Démo 2</option>
+            <option value="demo3">Démo 3</option>
+          </select>
+        </div>
       </div>
     );
   }
