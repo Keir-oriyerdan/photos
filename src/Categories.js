0254dac185bfde0d4b67145685a355f2a8d0f170
Keir <mcglencoe@hotmail.fr>
2020-11-21 00:25:11 +0100

    Test to "route" links from categories.js

--- src/Categories.js
+++ src/Categories.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./Categories.css";
+import Nature from "./Nature";
+import Medieval from "./Medieval";
+import Wedding from "./Wedding";
+import Portraits from "./Portraits";
+import Events from "./Events";
+import Shows from "./Shows";
+import Concerts from "./Concerts";
+import Personal from "./Personal";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 let urls = [
   "concerts.png",
@@ -22,6 +31,32 @@ function Categories() {
             className="categories-image"
             alt="categories"
           />
+          <Router>
+            <Route exact path="/nature.png">
+              <Nature />
+            </Route>
+            <Route exact path="/medieval">
+              <Medieval />
+            </Route>
+            <Route exact path="/wedding">
+              <Wedding />
+            </Route>
+            <Route exact path="/portraits">
+              <Portraits />
+            </Route>
+            <Route exact path="/events">
+              <Events />
+            </Route>
+            <Route exact path="/shows">
+              <Shows />
+            </Route>
+            <Route exact path="/concerts">
+              <Concerts />
+            </Route>
+            <Route exact path="/personal">
+              <Personal />
+            </Route>
+          </Router>
         </div>
       ))}
     </div>
