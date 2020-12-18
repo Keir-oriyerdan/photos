d3ebbf0b8b552fa05ebc0a08dc1beeb1dc80a2ae
Keir <mcglencoe@hotmail.fr>
2020-12-18 14:17:51 +0100

    Add RS-menu on footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Footer.css";
+import fb from "./images/facebook.png";
+import twitter from "./images/twitter.png";
+import YT from "./images/youtube.png";
+import Insta from "./images/instagram.png";
 
 function Footer() {
   return (
     <footer>
       <p>What the fuck</p>
+      <div className="rs-container">
+        <img className="rsicon" src={fb} alt="fb" />
+
+        <img className="rsicon" src={twitter} alt="twitter" />
+
+        <img className="rsicon" src={Insta} alt="insta" />
+
+        <img className="rsicon" src={YT} alt="yt" />
+      </div>
     </footer>
   );
 }
