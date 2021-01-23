3f804f3965f91838b9a28a5cc45662a637d90f93
Keir <mcglencoe@hotmail.fr>
2021-01-23 23:05:04 +0100

    add photos to gallery2

--- src/Gallery2.js
+++ src/Gallery2.js
@@ -2,8 +2,50 @@ import React from "react";
 import "./Gallery2.css";
 
 let columns = [
-  ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"],
-  ["07.jpg", "08.jpg", "09.jpg", "10.jpg", "11.jpg", "12.jpg"],
+  [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "16.jpg",
+    "17.jpg",
+    "18.jpg",
+  ],
+  [
+    "07.jpg",
+    "08.jpg",
+    "09.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+    "15.jpg",
+  ],
+  [
+    "19.jpg",
+    "20.jpg",
+    "21.jpg",
+    "22.jpg",
+    "23.jpg",
+    "24.jpg",
+    "25.jpg",
+    "26.jpg",
+    "27.jpg",
+  ],
+  [
+    "28.jpg",
+    "29.jpg",
+    "30.jpg",
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "11.jpg",
+    "12.jpg",
+  ],
 ];
 
 function Gallery2() {
