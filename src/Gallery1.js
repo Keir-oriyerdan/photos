313e9a3385b52446516dab08d2cdc4cf00922347
Keir <mcglencoe@hotmail.fr>
2021-01-28 23:47:35 +0100

    Add photos + update Gallery.Js

--- src/Gallery1.js
+++ src/Gallery1.js
@@ -1,59 +1,31 @@
 import React from "react";
-import "./Gallery1.css";
-import { PopupboxManager } from "react-popupbox";
+import "./Gallery2.css";
+import { SRLWrapper } from "simple-react-lightbox";
 
-let urls = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "04.jpg",
-  "05.jpg",
-  "06.jpg",
-  "07.jpg",
-  "08.jpg",
-  "09.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-  "13.jpg",
-  "14.jpg",
-  "15.jpg",
-  "16.jpg",
-  "17.jpg",
+let columns = [
+  ["01.jpg", "28.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"],
+  ["24.jpg", "22.jpg", "27.jpg", "10.jpg", "11.jpg", "12.jpg"],
+  ["13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg"],
+  ["19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg"],
+  ["25.jpg", "26.jpg", "27.jpg", "28.jpg", "07.jpg", "08.jpg"],
 ];
 
 function Gallery1() {
   return (
-    <div id="gallery">
-      {urls.map((url) => (
-        <div className="item">
-          <img src={"./gallery1/" + url} className="gallery1" alt="" />
+    <div className="gallery">
+      <SRLWrapper>
+        <div className="row">
+          {columns.map((column) => (
+            <div className="column">
+              {column.map((url) => (
+                <img src={"./gallery1/" + url} alt="" />
+              ))}
+            </div>
+          ))}
         </div>
-      ))}
+      </SRLWrapper>
     </div>
   );
 }
 
-export class Example extends Gallery1 {
-  openPopupbox() {
-    const content = (
-      <div>
-        <p className="quotes">Work like you don't need the money.</p>
-        <p className="quotes">Dance like no one is watching.</p>
-        <p className="quotes">And love like you've never been hurt.</p>
-        <span className="quotes-from">― Mark Twain</span>
-      </div>
-    );
-    PopupboxManager.open({ content });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.openPopupbox}>Click me</button>
-        <Gallery1 />
-      </div>
-    );
-  }
-}
 export default Gallery1;
