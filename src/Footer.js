f46ba351d7530fd638148c8133f3d4ca197108bf
Keir <mcglencoe@hotmail.fr>
2021-01-30 00:37:59 +0100

    update footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,20 +4,42 @@ import fb from "./images/facebook.png";
 import twitter from "./images/twitter.png";
 import YT from "./images/youtube.png";
 import Insta from "./images/instagram.png";
+import tumblr from "./images/tumblr.png";
 
 function Footer() {
   return (
     <footer>
       <p>What the fuck</p>
-      <div className="rs-container2">
-        <img className="rsicon" src={fb} alt="fb" />
-
-        <img className="rsicon" src={twitter} alt="twitter" />
-
-        <img className="rsicon" src={Insta} alt="insta" />
-
-        <img className="rsicon" src={YT} alt="yt" />
-      </div>
+      <ul className="rs-container2">
+        <li>
+          <a className="link" href="https://www.facebook.com/julhorn">
+            <img className="rsicon" src={fb} alt="fb" />
+          </a>
+        </li>
+        <li>
+          <a className="link" href="https://twitter.com/KeirORiyerdan">
+            <img className="rsicon" src={twitter} alt="twitter" />
+          </a>
+        </li>
+        <li>
+          <a className="link" href="https://www.instagram.com/julie__horn/">
+            <img className="rsicon" src={Insta} alt="insta" />
+          </a>
+        </li>
+        <li>
+          <a className="link" href="https://www.youtube.com/c/KeirORiyerdan">
+            <img className="rsicon" src={YT} alt="yt" />
+          </a>
+        </li>
+        <li>
+          <a
+            className="link"
+            href="https://ba-mhaith-liom-mo-saoirse.tumblr.com/"
+          >
+            <img className="rsicon" src={tumblr} alt="tumblr" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
